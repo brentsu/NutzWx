29be2059ddc77aa97af62457504c8ed3416c124a
Wizzercn <wizzer@qq.com>
2014-09-11 11:44:57 +0800

    调UI

--- src/main/webapp/themes/ace/assets/scripts/sys/unit.js
+++ src/main/webapp/themes/ace/assets/scripts/sys/unit.js
@@ -7,14 +7,15 @@ var Unit = {
     init: function () {
         this.handleUnit();
     },
-    handleUnit:function(){
+    handleUnit: function () {
+        //初始化树
         setting = {
             async: {
                 enable: true,
-                url:APP_BASE+"/private/sys/unit/list",
-                autoParam:["id"]
+                url: APP_BASE + "/private/sys/unit/list",
+                autoParam: ["id"]
             },
-            view:{
+            view: {
                 nameIsHTML: true
             },
             callback: {
@@ -22,26 +23,74 @@ var Unit = {
             }
         };
         this.initTree();
+        //删除
+        //重写dialog的title，运行加载html
+        $.widget("ui.dialog", $.extend({}, $.ui.dialog.prototype, {
+            _title: function (title) {
+                var $title = this.options.title || '&nbsp;'
+                if (("title_html" in this.options) && this.options.title_html == true)
+                    title.html($title);
+                else title.text($title);
+            }
+        }));
+        $("#btnDel").on('click', function (e) {
+            e.preventDefault();
+            if ($('#unitid').val() == '') {
+                $.gritter.add({
+                    title: '提示',
+                    text: '请选择一个单位',
+                    class_name: 'gritter-error gritter-center',
+                    time: 600
+                });
+                return false;
+            }
+            $("#dialogDel").removeClass('hide').dialog({
+                resizable: false,
+                modal: true,
+                title: "<div class='widget-header'><h4 class='smaller'><i class='icon-warning-sign red'></i> 删除单位？</h4></div>",
+                title_html: true,
+                buttons: [
+                    {
+                        html: "<i class='icon-trash bigger-110'></i>&nbsp; 删除",
+                        "class": "btn btn-danger btn-xs",
+                        click: function () {
+                            $(this).dialog("close");
+                        }
+                    }
+                    ,
+                    {
+                        html: "<i class='icon-remove bigger-110'></i>&nbsp; 取消",
+                        "class": "btn btn-xs",
+                        click: function () {
+                            $(this).dialog("close");
+                        }
+                    }
+                ]
+            });
+        });
+
+
     },
-    initTree:function(){
-        ztree =jQuery.fn.zTree.init(jQuery("#tree1"), setting);
+    initTree: function () {
+        ztree = jQuery.fn.zTree.init(jQuery("#tree1"), setting);
     },
-    zTreeOnAsyncSuccess:function(){
-        var tid=$("#id").html().replaceAll("&nbsp;","");
+    zTreeOnAsyncSuccess: function () {
+        var tid = $("#unitid").val();
         ztree.selectNode(ztree.getNodeByParam("id", tid, null));
-        if(tid!=""){
-            ztree.expandNode(ztree.getNodeByParam("id", tid.substring(0,tid.length-4), null), true);
+        if (tid != "") {
+            ztree.expandNode(ztree.getNodeByParam("id", tid.substring(0, tid.length - 4), null), true);
         }
     },
-    view:function(id){
+    view: function (id) {
+        $('#unitid').val(id);
         $.ajax({
             type: "POST",
             url: APP_BASE + "/private/sys/unit/view",
-            data: {"id":id},
+            data: {"id": id},
             dataType: "json",
             success: function (data) {
                 //清空内容
-                $('.widget-box').find(".editable").each(function(i){
+                $('.widget-box').find(".editable").each(function (i) {
                     if ($(this).prop('tagName') == "INPUT") {
                         $(this).val("");
                     } else {
@@ -49,7 +98,7 @@ var Unit = {
                     }
                 });
                 //赋值
-                $.each(data, function(index, value) {
+                $.each(data, function (index, value) {
                     if ($('#' + index).prop('tagName') == "INPUT") {
                         $('#' + index).val(value);
                     } else {
